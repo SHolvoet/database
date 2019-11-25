eedbb9cebd6dbe8e9046ffa7db0e841b0dcd1f84
jan moenens <janzinemail@hotmail.com>
2019-11-25 15:17:20 +0100

    Eerste commit

---
+++ Databaseexamen/dbo/Tables/Item.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Item] (
+    [ItemId]       INT        IDENTITY (1, 1) NOT NULL,
+    [Name]         NCHAR (50) NOT NULL,
+    [SlotType]     INT        NULL,
+    [AttackPower]  INT        NULL,
+    [DefencePower] INT        NULL,
+    [Health]       INT        NULL,
+    [Stamina]      INT        NULL,
+    [Mana]         INT        NULL,
+    PRIMARY KEY CLUSTERED ([ItemId] ASC)
+);
+
