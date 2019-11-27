07f81350ea7bd3bb3ab2fea617788ea197ce1cdb
jan moenens <55406662+janmoenens@users.noreply.github.com>
2019-11-27 20:40:22 +0100

    update

--- Databaseexamen/dbo/Tables/Item.sql
+++ Databaseexamen/dbo/Tables/Item.sql
@@ -9,5 +9,9 @@
     [Stamina]      INT        NULL,
     [Mana]         INT        NULL,
     PRIMARY KEY CLUSTERED ([ItemId] ASC), 
-    CONSTRAINT [CK_Item_SlotType] CHECK (AvatarType = NULL OR (SlotType = 1) OR (SlotType = 2) OR (SlotType = 4)) 
+    CONSTRAINT [CK_Item_SlotType] CHECK (AvatarType = NULL OR 
+												(SlotType = 1) OR 
+												(SlotType = 2) OR 
+												(SlotType = 4) OR 
+												(SlotType = 8))
 );
