e551f576ee681565454678292a9cbeaf07b64d1e
jan moenens <55406662+janmoenens@users.noreply.github.com>
2019-11-27 18:40:35 +0100

    eerste checkconstrain

--- Databaseexamen/dbo/Tables/Item.sql
+++ Databaseexamen/dbo/Tables/Item.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[Item] (
     [ItemId]       INT        IDENTITY (1, 1) NOT NULL,
     [Name]         NCHAR (50) NOT NULL,
+	[AvatarType]   INT		  NULL,
     [SlotType]     INT        NULL,
     [AttackPower]  INT        NULL,
     [DefencePower] INT        NULL,
     [Health]       INT        NULL,
     [Stamina]      INT        NULL,
     [Mana]         INT        NULL,
-    PRIMARY KEY CLUSTERED ([ItemId] ASC)
+    PRIMARY KEY CLUSTERED ([ItemId] ASC), 
+    CONSTRAINT [CK_Item_SlotType] CHECK (AvatarType = NULL OR (SlotType = 1) OR (SlotType = 2) OR (SlotType = 4)) 
 );
-
